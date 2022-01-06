7deafc885cf1827b53d7881c9c6283c65729b33c
alfael <alfael.sc@gmail.com>
2022-01-06 10:23:51 +0100

    Import AnyKernel package

---
+++ package/anykernel.sh
@@ -0,0 +1,40 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=BasiK by Alfael @ xda-developers
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=1
+device.name1=tissot
+supported.versions=10, 10.0, 11, 11.0, 12, 12.0
+supported.patchlevels=2019-06 -
+'; } # end properties
+
+# shell variables
+block=/dev/block/platform/soc/7824900.sdhci/by-name/boot;
+is_slot_device=1;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+
+## AnyKernel install
+dump_boot;
+
+
+
+write_boot;
