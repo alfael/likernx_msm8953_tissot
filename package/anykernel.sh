d205c7930e4c16909b9f6e2bff1dacef18a13579
alfael <alfael.sc@gmail.com>
2022-05-13 10:44:49 +0200

    Project rename to LIKERNX

--- package/anykernel.sh
+++ package/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=BasiK by Alfael @ xda-developers
+kernel.string=LIKERNX by Alfael @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.systemless=1
